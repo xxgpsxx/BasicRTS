bb142d8fda7e3716daab7707c3e7b5aa200a6814
xxgpsxx <gps360.infinityx@gmail.com>
2018-06-12 10:43:52 -0400

    Update MeleeUnit.java

--- MeleeUnit.java
+++ MeleeUnit.java
@@ -1,59 +1,33 @@
 import java.util.ArrayList;
-public class RangedUnit extends Unit
+import java.awt.geom.*;
+import javafx.event.*;
+import javafx.scene.input.*;
+public class MeleeUnit extends Unit
 {
-	private double range = 0;
-	private double projectileSpeed = 10.0;
 	private int counter = 0;
-	public RangedUnit(Map map, int health, int armor, Location location, int owner, double vision, double speed, int damage, double radius, double attackSpeed, double range, double projectileSpeed)
+	public MeleeUnit(Map map, int health, int armor, Location location, int owner, double vision, double speed, int damage, double radius, double attackSpeed)
 	{
 		super(map, health, armor, location, owner, vision, speed, damage, radius, attackSpeed);
-		this.range = range;
-		this.projectileSpeed = projectileSpeed;
 	}
-	public RangedUnit(Map map, int health, int armor, Location location, double vision, double speed, int damage, double radius, double attackSpeed, double range, double projectileSpeed)
+	public MeleeUnit(Map map, int health, int armor, Location location, double vision, double speed, int damage, double radius, double attackSpeed)
 	{
 		super(map, health, armor, location, vision, speed, damage, radius, attackSpeed);
-		this.range = range;
-		this.projectileSpeed = projectileSpeed;
 	}
-	public RangedUnit(int health, int armor, Location location, double vision, double speed, int damage, double radius, double attackSpeed, double range, double projectileSpeed)
+	public MeleeUnit(int health, int armor, Location location, double vision, double speed, int damage, double radius, double attackSpeed)
 	{
 		super(health, armor, location, vision, speed, damage, radius, attackSpeed);
-		this.range = range;
-		this.projectileSpeed = projectileSpeed;
 	}
-	public double getProjectileSpeed() { return projectileSpeed; }
 	public double getVision() { return super.getVision(); }
 	public int setOwner(int x) { return super.setOwner(x); }
 	public Map setMap(Map map) { return super.setMap(map); }
-	public Map getMap() { return super.getMap(); }
-	public double getRange() { return range; }
 	public int getMaxHealth() { return super.getMaxHealth(); }
-	public Boolean inRange(Thing other)
-	{
-		return range >= getLocation().distanceFrom(other.getCenter());
-	}
 	public void attackMove()
 	{
-		ArrayList <Thing> thingsInVision = super.getThingsInVision();
+		Thing target = getClosestHostile();
 
-		Thing target = null;
-		double minDistance = 100000.0;
-
-		for(Thing thing : thingsInVision)
-		{
-			if(thing.getOwner() != getOwner())
-			{
-				if(getLocation().distanceFrom(thing.getCenter()) < minDistance)
-				{
-					target = thing;
-					minDistance = getLocation().distanceFrom(thing.getCenter());
-				}
-			}
-		}
 		if(target != null)
 		{
-			if(inRange(target))
+			if(inContact(target))
 			{
 				stop();
 				attack();
@@ -65,17 +39,20 @@ public class RangedUnit extends Unit
 				}
 				counter--;
 			}
-
 			else
 			{
 				moveTo(target.getLocation());
 				super.move();
 			}
+
 		}
-		else if(target != null)
+		else
 		{
-			moveTo(getTarget());
-			super.move();
+			if(super.getTarget() != null)
+			{
+				moveTo(getTarget());
+				super.move();
+			}
 		}
 	}
 
